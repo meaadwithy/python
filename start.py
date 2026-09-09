7c0d6a811af5f18f1b1162fd2edb03a0fd29ecca
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-09-09 04:20:05 +0300

    Add various print statements and operations in start.py

---
+++ start.py
@@ -0,0 +1,27 @@
+
+
+name = "meaad"
+age = 20
+print(name)
+print(type(age))
+
+print('''"Oh no", she exclaimed, "Ben's bike is broken!"''')
+print("""'Oh no', she exclaimed, 'Ben's bike is broken!'""")
+
+# for comment use #
+
+print(10 / 5) # result double not int 2.0
+
+print(9 // 5) # result ignores the remainder 1
+print(9 % 5) # result module int 4
+print(7 // 3)    # This is the integer division operator
+print(7 % 3)     # This is the remainder or modulus operator
+
+
+print(square(3)) # give 9
+print(sub(6, 4)) # subtract  6-4 =2
+
+print(3.9999, int(3.9999)) # This doesn't round to the closest int!
+print(3.0, int(3.0))
+print(float("123.45"))
+print(str(17))
