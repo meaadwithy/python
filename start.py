ff6f64852997fa14fa02d2ba6ca53666b53ff5d8
Meaadwithy <meaad.alqthmi145@gmail.com>
2026-09-09 04:38:39 +0300

    Enhance start.py with comments and print statements
    Added comments to clarify code functionality and behavior.

--- start.py
+++ start.py
@@ -2,14 +2,16 @@
 
 name = "meaad"
 age = 20
-print(name)
+print(name) # type write in python str not string
 print(type(age))
+print(len("hello"))
 
 print('''"Oh no", she exclaimed, "Ben's bike is broken!"''')
 print("""'Oh no', she exclaimed, 'Ben's bike is broken!'""")
 
 # for comment use #
 
+--------------------
 print(10 / 5) # result double not int 2.0
 
 print(9 // 5) # result ignores the remainder 1
@@ -21,7 +23,15 @@ print(7 % 3)     # This is the remainder or modulus operator
 print(square(3)) # give 9
 print(sub(6, 4)) # subtract  6-4 =2
 
+print(2 ** 3 ** 2)     # the right-most ** operator gets done first! so 3 ** 2 give 9 then 2 ** 9 give 512  
+print((2 ** 3) ** 2)   # use parentheses to force the order you want! its same idea as int to the power of int
+
 print(3.9999, int(3.9999)) # This doesn't round to the closest int!
 print(3.0, int(3.0))
 print(float("123.45"))
 print(str(17))
+
+-------------------
+n = input("Please enter your name: ")
+print("Hello", n)
+
